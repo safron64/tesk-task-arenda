1156c54dceb7c3c4002e71cbb6473b8f63d65ca6
Nikita <ed.kahr.19063@gmail.com>
2024-06-01 15:23:38 +0300

    first-commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const menuBtn = document.getElementById('menu-btn')
+	const nav = document.getElementById('nav')
+	const body = document.body
+
+	menuBtn.addEventListener('click', () => {
+		// Переключаем класс 'open' для меню и кнопки
+		nav.classList.toggle('open')
+		menuBtn.classList.toggle('open')
+		body.classList.toggle('no-scroll')
+	})
+})
